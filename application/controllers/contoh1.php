d93adfb17c2486ccc5876c0d2625b224e784b5a7
daniellasroha <daniellasrohasiahaan@gmail.com>
2023-09-25 16:24:56 +0700

    menambahkan pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Daniel Lasroha Siahaan hobi saya bermain game. Dan saya berkuliah di UBSI NIM Saya adalah 19221036";
+    }
+}
